8da786c5344b3a51bb522a2e88a36e447cc9342a
Mayoogh <mayoogh1997@gmail.com>
2020-08-20 23:41:23 +0530

    Added Mechanical matrix keypad

---
+++ Matrix Keypad/3X4_Mechanical_keypad_Matrix/3X4_Mechanical_keypad_Matrix.ino
@@ -0,0 +1,33 @@
+// Reference : http://domoticx.com/arduino-keypad-4x3/
+
+
+#include <Keypad.h>
+
+const byte ROWS = 4; //four rows
+const byte COLS = 3; //three columns
+
+char keys[ROWS][COLS] = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
+};
+byte rowPins[ROWS] = {7, 2, 3, 5}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {6, 8, 4}; //connect to the column pinouts of the keypad
+
+//Create an object of keypad
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+void setup(){
+  Serial.begin(9600);
+}
+  
+void loop(){
+  char key = keypad.getKey();// Read the key
+  
+  // Print if key pressed
+  if (key){
+    Serial.print("Key Pressed : ");
+    Serial.println(key);
+  }
+}
